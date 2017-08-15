7810e0f84defd271d9c9d7b140575019befd9eec
Andrew Greenough <andygreenough@gmail.com>
2017-08-15 13:30:48 +0100

    actions and outlets

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andrew Greenough on 15/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
